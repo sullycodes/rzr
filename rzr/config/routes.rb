f933e6ec30e39cc98e613060d2d42b6d8462b36b
Dan Sullivan <dansullivan19@gmail.com>
2020-07-21 19:56:00 -0400

    scrape button

--- rzr/config/routes.rb
+++ rzr/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :estimates
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   resources :estimates do
-    match '/scrape' to: 'estimates#scrape', via: :post, on: :collection
+    match '/scrape', to: 'estimates#scrape', via: :post, on: :collection
   end
 
   root to: 'estimates#index'

6182a35e2a53dc86d2134ab0b458f2134eb7c384
Dan Sullivan <dansullivan19@gmail.com>
2020-07-28 17:08:42 -0400

    scrape contoller setup

--- rzr/app/controllers/estimates_controller.rb
+++ rzr/app/controllers/estimates_controller.rb
@@ -61,17 +61,18 @@ class EstimatesController < ApplicationController
     end
   end
 
-  # def scrape
-  #   url = 'https://www.cars.com/shopping/sedan/'
-  #   response = VehiclesSpider.process(url)
-  #   if response[:status] == :completed && response[:error].nil?
-  #     flash.now[:notice] = "Successfully scraped url"
-  #   else
-  #     flash.now[:alert] = response[:error]
-  #   end
-  # rescue StandardError => e
-  #   flash.now[:alert] = "Error: #{e}"
-  # end
+  def scrape
+    address = params[:address]
+    zillow_scrape= 'https://www.zillow.com/homes/11-Taylor-St-Gloucester,-MA,-01930_rb'
+    response = EstimatesSpider.process(url)
+    if response[:status] == :completed && response[:error].nil?
+      flash.now[:notice] = "Successfully scraped url"
+    else
+      flash.now[:alert] = response[:error]
+    end
+  rescue StandardError => e
+    flash.now[:alert] = "Error: #{e}"
+  end
 
 
 

4a5885de9dfd6b271d06c126c2fe97d7e450fcc0
Dan Sullivan <dansullivan19@gmail.com>
2020-07-28 17:03:38 -0400

    address input

--- rzr/app/controllers/estimates_controller.rb
+++ rzr/app/controllers/estimates_controller.rb
@@ -61,6 +61,20 @@ class EstimatesController < ApplicationController
     end
   end
 
+  # def scrape
+  #   url = 'https://www.cars.com/shopping/sedan/'
+  #   response = VehiclesSpider.process(url)
+  #   if response[:status] == :completed && response[:error].nil?
+  #     flash.now[:notice] = "Successfully scraped url"
+  #   else
+  #     flash.now[:alert] = response[:error]
+  #   end
+  # rescue StandardError => e
+  #   flash.now[:alert] = "Error: #{e}"
+  # end
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_estimate
